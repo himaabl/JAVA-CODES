f632905ec77236c0f8f4493fe5cfa39a38817e39
Himanshi Ablankar <ablankarhimanshi@gmail.com>
2023-07-23 19:56:28 +0530

    added type promo

--- ADD.java
+++ ADD.java
@@ -1,7 +1,11 @@
 public class ADD {
     public static void main(String [] args)
     {
-        System.out.println(3+5);
+       //byte b= 125;
+       int a= 12;
+       byte k = (byte)a;
+
+       System.out.println(a);
     }
  
 }
---
+++ Type.java
@@ -0,0 +1,12 @@
+public class Type {
+    public static void main (String [] args)
+    {
+         byte a= 10;
+           byte b= 30;      
+
+    int result = a*b;
+    System.out.println(result);
+    // type promotion
+    }
+ 
+}
